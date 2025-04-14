04bf4ba6758a4cca31c9add60a0af55c47ceb0ec
Aidos95 <kuralbaev777@gmail.com>
2025-04-14 11:43:08 +0500

    Update main.py

--- main.py
+++ main.py
@@ -158,4 +158,7 @@ async def main():
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        print("⛔️ Бот остановлен")
